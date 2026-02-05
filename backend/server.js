59b1a87082bcece5d8c97b39d4e4fffea4fbacb2
Abdallahi Nah <abdallahi.nah.un.fst@gmail.com>
2026-02-05 11:04:37 +0000

    first commit

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "https://project-management-system-onyc.vercel.app/",
+      "https://project-management-system-onyc.vercel.app",
       "http://localhost:3000",
       "http://localhost:5173",
     ],
